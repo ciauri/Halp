c25386682e6b8909f631c85727c08201f35efdf9
Stephen Ciauri <stephen.ciauri@americanwell.com>
2019-08-15 15:04:29 -0700

    Adding repro projects
    Add gitignore and import
    
    Adding repro projects
    
    Add gitignore and import
    
    remove gitignored

---
+++ App/App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Stephen Ciauri on 8/15/19.
+//  Copyright © 2019 Don't Worry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StaticLib/StaticLib/StaticLib.swift
@@ -0,0 +1,13 @@
+//
+//  StaticLib.swift
+//  StaticLib
+//
+//  Created by Stephen Ciauri on 8/15/19.
+//  Copyright © 2019 Don't Worry. All rights reserved.
+//
+
+import Package
+
+class StaticLib {
+
+}
